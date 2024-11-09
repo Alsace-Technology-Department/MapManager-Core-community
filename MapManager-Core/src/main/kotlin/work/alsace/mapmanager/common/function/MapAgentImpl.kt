3409e18b100f96bfa4e098abe29419ecd1bd7aec
Kyle Ma <m2069134445@icloud.com>
2024-11-09 20:26:06 +0800

    fix: 修复爆炸破坏的显示问题

--- MapManager-Core/src/main/kotlin/work/alsace/mapmanager/common/function/MapAgentImpl.kt
+++ MapManager-Core/src/main/kotlin/work/alsace/mapmanager/common/function/MapAgentImpl.kt
@@ -669,15 +669,16 @@ class MapAgentImpl(private val plugin: MapManagerImpl) : MapAgent {
      *
      * @param world 世界名称。
      * @return 指定世界的物理规则状态。
+     *
      */
     override fun isPhysical(world: String): Boolean {
         return getWorldNode(world)?.physical == true
     }
 
     /**
-     * 设置全局爆炸保护状态。
+     * 设置全局爆炸破坏状态。
      *
-     * @param exploded 新的爆炸保护状态。
+     * @param exploded 新的爆炸破坏状态。
      */
     override fun setExploded(exploded: Boolean?) {
         Companion.exploded = exploded
@@ -686,20 +687,20 @@ class MapAgentImpl(private val plugin: MapManagerImpl) : MapAgent {
     }
 
     /**
-     * 设置指定世界的爆炸保护状态。
+     * 设置指定世界的爆炸破坏状态。
      *
      * @param world 世界名称。
-     * @param exploded 新的爆炸保护状态。
+     * @param exploded 新的爆炸破坏状态。
      */
     override fun setExploded(world: String, exploded: Boolean) {
         getWorldNode(world)?.exploded = exploded
     }
 
     /**
-     * 获取指定世界的爆炸保护状态。
+     * 获取指定世界的爆炸破坏状态。
      *
      * @param world 世界名称。
-     * @return 指定世界的爆炸保护状态。
+     * @return 指定世界的爆炸破坏状态。
      */
     override fun isExploded(world: String): Boolean {
         return getWorldNode(world)?.exploded == true
